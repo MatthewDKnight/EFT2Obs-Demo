aac97bdd903218b5a56850c61a3dead7bed58009
Matthew Knight <mdk16@lx02.hep.ph.ic.ac.uk>
2019-11-07 16:08:56 +0000

    extract_coeff now includes the >200 bin

--- extract_coeff.py
+++ extract_coeff.py
@@ -2,55 +2,50 @@ import yoda
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+from parameter_conversion import params as param_convert #dictionary to switch naming conventions
 
 def getBins(hist):
-	bin_centres = []
 	bin_values = []
-	widths = []
-	errors = []	
+       
+	#merge bins into 0,20,45,80,120,200,400 
+	hist.mergeBins(0,3)
+	hist.mergeBins(1,5)
+	hist.mergeBins(2,8)
+	hist.mergeBins(3,10)
+	hist.mergeBins(4,19)
+	hist.mergeBins(5,len(hist)-1)
 	
-	bin_value = 0
-	sum_error2 = 0
-	for small_bin in hist:
-		small_edges = small_bin.xEdges
-		if small_edges[1] in bin_edges:
-			bin_value = bin_value + small_bin.area
-			sum_error2 = sum_error2 + small_bin.areaErr**2
-			bin_values.append(bin_value)
-			errors.append(np.sqrt(sum_error2))
-			bin_value = 0
-			sum_error2 = 0
-			
-			outer_edge_index = bin_edges.index(small_edges[1])
-			edges = [bin_edges[outer_edge_index-1], bin_edges[outer_edge_index]]
-			bin_centre = (float(edges[0])+float(edges[1]))/2
-			bin_centres.append(bin_centre)
+	for bin in hist:
+		bin_values.append(bin.sumW)
+	
+	overflow = hist.overflow.sumW
+	bin_values[-1] += overflow
 
-			width = edges[1] - edges[0]
-			widths.append(width)
-		else:
-			bin_value = bin_value + small_bin.area
-			sum_error2 = sum_error2 + small_bin.areaErr**2
-
-	return bin_centres, bin_values, widths, errors
+	return bin_values
+	
 
 def plot(hist):
-	bin_centres, bin_values, widths, errors = getBins(hist)
+	bin_values = getBins(hist)
 	bin_values = np.array(bin_values)
 	norm = bin_values[0]
 	bin_values = (bin_values/norm) * 27.45
 	
-	errors = np.array(errors)
-	errors = (errors/norm) * 27.45
+	widths = [] 
+	for i in range(len(bin_edges)-1):
+		widths.append(bin_edges[i+1]-bin_edges[i])
 
-	print(bin_values)
-	print(errors)
-	plt.bar(bin_centres, bin_values, widths, align="center")
-	plt.errorbar(bin_centres, bin_values, yerr=errors)
+	left = bin_edges[:len(bin_edges)-1] #remove last bin edge
+	height = bin_values[:len(bin_values)-1] #remove >200 bin value
+
+	print(left)
+	print(height)
+	print(widths)
+
+	plt.bar(left, height, widths, align="edge")
 	plt.show()
 
 def extractCoeff(hists):
-	bin_values = [getBins(hist)[1] for hist in hists] #values of bins for each reweighting
+	bin_values = [getBins(hist) for hist in hists] #values of bins for each reweighting
 	bin_values = np.array(bin_values)
 	bin_values = bin_values/bin_values[0] #divide by SM value -> yields
 	bin_values = bin_values - 1 #now left with INT and BSM terms
@@ -58,8 +53,8 @@ def extractCoeff(hists):
 	no_params = len(pars)
 	no_bins = len(bin_values[0])
 	
-	print(no_params)
-	print(no_bins)
+	#print(no_params)
+	#print(no_bins)
 
 	#create a to carry all coefficients of for every bin
 	coeffs = [ [[0.0,0.0] for i in range(no_params)] for j in range(no_bins)]
@@ -88,21 +83,21 @@ def writeTextFile(coeffs):
 			bin = coeffs[j]
 			string = "%s:1"%bin_names[j]
 			for i in range(len(bin)):
-				param = pars[i]['name']
+				param_name = param_convert[pars[i]['name']]
 				A = bin[i][0]
 				B = bin[i][1]
 				#make exception for cG and cA parameters
-				if param in ["cG","tcG", "cA"]:
+				if param_name in ["cG","tcG", "cA"]:
 					A = A / (4*np.pi)**2
 					B = B / (4*np.pi)**4
 				if A>0:
-					string += " + %f * %s"%(A, param)
+					string += " + %f * %s"%(A, param_name)
 				else:
-					string += " %f * %s"%(A, param)
+					string += " %f * %s"%(A, param_name)
 				if B>0:
-					string += " + %f * %s * %s"%(B, param, param)
+					string += " + %f * %s * %s"%(B, param_name, param_name)
 				else:
-					string += " %f * %s * %s"%(B, param, param)
+					string += " %f * %s * %s"%(B, param_name, param_name)
 			string += "\n"		
 			file.write(string)
 
@@ -112,10 +107,12 @@ hists = hists[1:] #get rid of first histogram
 
 #Bins for pT are 0,20,45,80,120,200
 bin_edges = [0,20,45,80,120,200]
-bin_names = ["GG2H_PTH_0_20", "GG2H_PTH_20_45", "GG2H_PTH_45_80", "GG2H_PTH_80_120", "GG2H_PTH_120_200"] 
+bin_names = ["GG2H_PTH_0_20", "GG2H_PTH_20_45", "GG2H_PTH_45_80", "GG2H_PTH_80_120", "GG2H_PTH_120_200", "GG2H_PTH_GT200"] 
 
 with open("config.json") as jsonfile:
 	pars = json.load(jsonfile)
 
 coeffs = extractCoeff(hists)
 writeTextFile(coeffs)
+
+#plot(hists[0])
