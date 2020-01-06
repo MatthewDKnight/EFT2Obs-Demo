fc5eba715a6c86e90b197f06ffabed713271ba77
Matthew Knight <mdk16@lx02.hep.ph.ic.ac.uk>
2020-01-06 11:51:38 +0000

    Find maximum acceptance effect

--- analysis-files/acceptances.py
+++ analysis-files/acceptances.py
@@ -1,51 +1,169 @@
 import matplotlib.pyplot as plt
 import yoda
 import numpy as np
+import sys
 
-aos=yoda.read("SimpleHiggs.yoda", asdict=False)
+def getHistNumber(hist):
+        split_str = hist.name.split("w")[1]
+        split_str = split_str[:len(split_str)-1]
+        return int(split_str)
 
-xs_list_no_acc=[]
-xs_list_acc=[]
+def orderHists(hists):
+        ordered = False
+        while ordered==False:
+                ordered=True
+                for i in range(len(hists)-1):
+                        if getHistNumber(hists[i]) > getHistNumber(hists[i+1]):
+                                #swap histograms
+                                ordered = False
+                                hists[i], hists[i+1] = hists[i+1], hists[i]
+
+aos=yoda.read(sys.argv[1], asdict=False)
+
+no_acc_hists = []
+acc_hists = []
 for i in aos:
-	#if 'H_PT[rw' in i.name:
-	if i.path.startswith("/SimpleHiggs/N_jets"):
-		bin_list_no_acc=[]
-		num_bins=len(i)
-		for j in range(len(i)):
-			#print(i[j])
-			bin_list_no_acc.append(i[j].sumW)
-		xs_list_no_acc.append(bin_list_no_acc)
-	if i.path.startswith("/SimpleHiggs/acc_N_jets"):
-		bin_list_acc=[]
-		for j in range(len(i)):
-			#print(i[j])
-			bin_list_acc.append(i[j].sumW)
-		xs_list_acc.append(bin_list_acc)
+	if i.path.startswith("/SimpleHiggs/H_PT"):
+		no_acc_hists.append(i)
+	elif i.path.startswith("/SimpleHiggs/acc_H_PT"):
+		acc_hists.append(i)
+no_acc_hists = no_acc_hists[1:]
+acc_hists = acc_hists[1:]
+orderHists(no_acc_hists)
+orderHists(acc_hists)	
 
-		
-xs_list_no_acc=xs_list_no_acc[1:4]
-xs_list_acc=xs_list_acc[1:4]
+pt_bin_edges = [0,20,45,80,120,200]
+pt_bin_names = ["GG2H_PTH_0_20", "GG2H_PTH_20_45", "GG2H_PTH_45_80", "GG2H_PTH_80_120", "GG2H_PTH_120_200", "GG2H_PTH_GT200"]
 
-cWW_values=[0.0, 0.5, 1.0]
+njet_bin_edges = [0,1,2,3,4]
+njet_bin_names = ["0j", "1j", "2j", "3j", ">3j"]
 
-plt.xlabel('cWW')
-plt.ylabel('Acceptance')
+bounds = {"cww":[-0.015,0.015], "chw":[-0.012,0.016], "cphl":[-0.05,0.05],"Cross":[-1.0,1.0]}
 
-x = np.linspace(0,1,10000)
+"""
+Plot the total acceptance as a function of the parameters to test for the form of the function
+"""
+SM_acc = acc_hists[0].sumW()/no_acc_hists[0].sumW()
+acceptance_arrays = {}
+params = ["cww","chw","cphl","Cross"]
+for i in range(len(params)):
+	param = params[i]
+	
+	no_acc_hists_temp = no_acc_hists[(1+(9*i)):(9*(i+1)+1)]
+	acc_hists_temp = acc_hists[(1+(9*i)):(9*(i+1)+1)]
 
+	no_acc_xs = []
+	acc_xs = []
+	for j in range(9):
+		no_acc_xs.append(no_acc_hists_temp[j].sumW())
+		acc_xs.append(acc_hists_temp[j].sumW())
+	no_acc_xs = np.array(no_acc_xs)
+	acc_xs = np.array(acc_xs)
+	
+	acceptance = acc_xs/no_acc_xs
+	normed_acceptance = acceptance / SM_acc
 
-for k in range(num_bins):
-	cWW_xs_values_no_acc=np.array([i[k] for i in xs_list_no_acc])
-	cWW_xs_values_acc=np.array([i[k] for i in xs_list_acc])
-	#cHW_xs_values=[xs_list[0], xs_list[3], xs_list[4]]
-	acceptance=cWW_xs_values_acc/cWW_xs_values_no_acc
-	#print(cWW_xs_values_acc)
-	acceptance_SM=acceptance[0]
-	normed_acceptance=acceptance/acceptance_SM
+	acceptance_arrays[param] = normed_acceptance
 
-	p = np.polyfit(cWW_values, normed_acceptance, 2)
-	print(p)
-	#plt.plot(cWW_values, normed_acceptance, 'bo')
-	#plt.plot(x, np.polyval(p,x))
-	#plt.show()
+	bound = bounds[param]
+	interval = bound[1] - bound[0]
+	x = [(bound[0] + (interval/8)*i) for i in range(9)]
 
+	plt.scatter(x, normed_acceptance)
+	plt.show()
+
+y1 = acceptance_arrays["cww"] + acceptance_arrays["chw"] + acceptance_arrays["cphl"] - 2
+y2 = acceptance_arrays["Cross"]
+plt.plot(x, y1, label="Total")
+plt.plot(x, y2, label="Cross")
+plt.legend()
+plt.show()
+
+"""
+Determine the maximum acceptance for each bin.
+"""
+
+pt_bin_edges = [0,20,45,80,120,200]
+pt_bin_names = ["GG2H_PTH_0_20", "GG2H_PTH_20_45", "GG2H_PTH_45_80", "GG2H_PTH_80_120", "GG2H_PTH_120_200", "GG2H_PTH_GT200"]
+njet_bin_edges = [0,1,2,3,4]
+njet_bin_names = ["0j", "1j", "2j", "3j", ">3j"]
+
+bin_names = pt_bin_names + njet_bin_names
+
+aos=yoda.read(sys.argv[1], asdict=False)
+
+H_PT_hists = [h for h in aos if h.path.startswith("/SimpleHiggs/H_PT")]
+H_PT_hists = H_PT_hists[1:] #get rid of first histogram
+orderHists(H_PT_hists)
+
+acc_H_PT_hists = [h for h in aos if h.path.startswith("/SimpleHiggs/acc_H_PT")]
+acc_H_PT_hists = acc_H_PT_hists[1:] #get rid of first histogram
+orderHists(acc_H_PT_hists)
+
+N_jets_hists = [h for h in aos if h.path.startswith("/SimpleHiggs/N_jets")]
+N_jets_hists = N_jets_hists[1:] #get rid of first histogram
+orderHists(N_jets_hists)
+
+acc_N_jets_hists = [h for h in aos if h.path.startswith("/SimpleHiggs/acc_N_jets")]
+acc_N_jets_hists = acc_N_jets_hists[1:] #get rid of first histogram
+orderHists(acc_N_jets_hists)
+
+params = ["cww","chw","cphl"]
+max_acceptances_holder = []
+for k in range(len(params)):
+        param = params[k]
+
+        H_PT_hists_temp = H_PT_hists[(1+(9*k)):(9*(k+1)+1)]
+        acc_H_PT_hists_temp = acc_H_PT_hists[(1+(9*k)):(9*(k+1)+1)]
+	N_jets_hists_temp = N_jets_hists[(1+(9*k)):(9*(k+1)+1)]
+        acc_N_jets_hists_temp = acc_N_jets_hists[(1+(9*k)):(9*(k+1)+1)]
+
+	max_acceptances = []
+	no_pt_bins = len(H_PT_hists_temp[0])
+	no_njet_bins = len(N_jets_hists_temp[0])
+	no_reweightings = 9
+
+	#find maximum acceptance for each bin
+	for i in range(no_pt_bins):
+		SM_acceptance = acc_H_PT_hists[0][i].sumW/H_PT_hists[0][i].sumW
+		acceptances = []
+		for j in range(no_reweightings):
+			acceptances.append(acc_H_PT_hists_temp[j][i].sumW/H_PT_hists_temp[j][i].sumW)
+		max_acceptances.append(max(acceptances)/SM_acceptance)
+	SM_acceptance = acc_H_PT_hists[0].overflow.sumW/H_PT_hists[0].overflow.sumW
+        acceptances = []
+        for j in range(no_reweightings):
+                acceptances.append(acc_H_PT_hists_temp[j].overflow.sumW/H_PT_hists_temp[j].overflow.sumW)
+        max_acceptances.append(max(acceptances)/SM_acceptance)
+
+	for i in range(no_njet_bins):
+		SM_acceptance = acc_N_jets_hists[0][i].sumW/N_jets_hists[0][i].sumW
+		acceptances = []
+		for j in range(no_reweightings):
+			acceptances.append(acc_N_jets_hists_temp[j][i].sumW/N_jets_hists_temp[j][i].sumW)
+		max_acceptances.append(max(acceptances)/SM_acceptance)
+	SM_acceptance = acc_N_jets_hists[0].overflow.sumW/N_jets_hists[0].overflow.sumW
+        acceptances = []
+        for j in range(no_reweightings):
+               acceptances.append(acc_N_jets_hists_temp[j].overflow.sumW/N_jets_hists_temp[j].overflow.sumW)
+        max_acceptances.append(max(acceptances)/SM_acceptance)
+	
+	print("------------------------------------")
+	print("Parameter: %s"%param)
+	for i in range(len(bin_names)):
+		print(bin_names[i], max_acceptances[i])
+
+	max_acceptances_holder.append(np.array(max_acceptances))
+
+#sum acceptance effect over all parameters
+max_acceptances = max_acceptances_holder[0] + max_acceptances_holder[1] + max_acceptances_holder[2] - 2
+print("---------------------------------")
+print("Total")
+for i in range(len(bin_names)):
+	print(bin_names[i], max_acceptances[i])
+
+with open("max_acceptances.txt", "w") as f:
+	for i in range(len(bin_names)):
+		string = bin_names[i] + ": %f"%max_acceptances[i] + "\n"
+		f.write(string)
+	
