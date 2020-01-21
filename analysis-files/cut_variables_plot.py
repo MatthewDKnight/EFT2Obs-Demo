f8855896d0b5a8bce2fc7d185cb841ccf1c5a5b6
Matthew Knight <mdk16@lx02.hep.ph.ic.ac.uk>
2020-01-21 11:22:54 +0000

    cut_variables_plot.py not account dependent

--- analysis-files/cut_variables_plot.py
+++ analysis-files/cut_variables_plot.py
@@ -2,6 +2,7 @@ import yoda
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
+import sys
 
 font = {'family' : 'normal',
         'weight' : 'normal',
@@ -9,10 +10,10 @@ font = {'family' : 'normal',
 
 matplotlib.rc('font', **font)
 
-aos = yoda.read("SimpleHiggs_old.yoda", asdict=False)
+aos = yoda.read(sys.argv[1], asdict=False)
 
-colours = {0:"red", 2:"blue", 4:"green", 5:"blue"}
-legend = {0:"SM", 1:"cWW", 2:"cWW", 3:"cHW", 4:"cHW"}
+colours = {0:"red", 2:"blue", 4:"green", 5:"blue",19:"blue"}
+legend = {0:"SM", 1:"cWW", 2:"cWW", 3:"cHW", 4:"cHW",19:"cross"}
 
 cut_points = {"leading_lep_PT":25,"trailing_lep_PT":13,
 	      "leading_lep_eta":2.5,"trailing_lep_eta":2.5,
@@ -32,7 +33,7 @@ def plot(hist, norm = True):
 		bin_values = np.array(bin_values)
 		bin_values = bin_values / np.sum(bin_values)
 
-	number = findReweightNumber(hist)
+	number = getHistNumber(hist)
 	
 	plt.bar(edges, bin_values, widths, align="edge",alpha=0.5, color = colours[number], label = legend[number])
 	return np.max(bin_values)
@@ -77,14 +78,31 @@ def findUniqueNames(hists):
 			names.append(name)
 	return names
 
+"""
 def findReweightNumber(hist):
 	rw_number = hist.name.split("[")[1]
 	number = rw_number[2]
         number = int(number)
 	return number
+"""
 
-exclude = ["H_PT", "N_jets", "lepton_no", "acceptance"]
+def getHistNumber(hist):
+        split_str = hist.name.split("w")[1]
+        split_str = split_str[:len(split_str)-1]
+        return int(split_str)
 
+def orderHists(hists):
+        ordered = False
+        while ordered==False:
+                ordered=True
+                for i in range(len(hists)-1):
+                        if getHistNumber(hists[i]) > getHistNumber(hists[i+1]):
+                                #swap histograms
+                                ordered = False
+                                hists[i], hists[i+1] = hists[i+1], hists[i]
+        return hists
+
+exclude = ["jet_PT", "H_PT", "N_jets", "lepton_no", "acceptance"]
 hists = []
 
 for hist in aos:
@@ -98,15 +116,19 @@ for hist in aos:
 
 names = findUniqueNames(hists)
 hists = hists[len(names):] #remove weight merging histograms
+orderHists(hists)
 
 def main():
-	for i in [2,4]:
+	for i in [19]:
 		wanted_reweights = [0,i]
 		for name in names:
 			plt.figure(figsize=(10,7))
 			hists_plotted = []
 			for hist in hists:
-				if (name in hist.name) and (findReweightNumber(hist) in wanted_reweights):
+				#print(name)
+				#print(hist.name)
+				#print(getHistNumber(hist))
+				if (name in hist.name) and (getHistNumber(hist) in wanted_reweights):
 					max = plot(hist)
 					hists_plotted.append(hist)
 			
@@ -128,7 +150,8 @@ def main():
 			plt.plot(x,y,"--",color="black",linewidth=3)
 
 			plt.xlabel(name)
-			plt.ylabel("P(%s) - P(SM)"%legend[findReweightNumber(hists_plotted[1])])
+			plt.ylabel("P(%s) - P(SM)"%legend[getHistNumber(hists_plotted[1])])
 			plt.savefig("cutVariablesPlots/%s%s_diff.png"%(name,i))
 			#plt.show()
 
+main()
